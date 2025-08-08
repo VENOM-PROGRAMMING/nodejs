0551e6d9677fbf59b640849472d9ff82e26533d3
tatjanatregub <tatjanatregub@proton.me>
2025-08-08 02:31:42 -0700

    'ok'

--- api/index.js
+++ api/index.js
@@ -1,18 +1,29 @@
-let stats = {
+let stats = {}; // global object
+
+const defaultStats = () => ({
   scanari: 0,
   tranzactii_noi: 0,
   transferuri: 0,
   error_transferuri: 0,
-};
+});
 
 export default function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
-    for (const key in data) {
-      if (stats.hasOwnProperty(key)) {
-        stats[key] += data[key];
+
+    for (const address in data) {
+      if (!stats[address]) {
+        stats[address] = defaultStats();
+      }
+      const updates = data[address];
+
+      for (const key in updates) {
+        if (stats[address].hasOwnProperty(key)) {
+          stats[address][key] += updates[key];
+        }
       }
     }
+
     res.status(200).json({ message: "Stats updated", stats });
   } else if (req.method === "GET") {
     res.status(200).json(stats);
